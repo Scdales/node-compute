482d7f21e4192076a5ca1a463542a27e8ce871e4
Scdales <seancliffdaley@gmail.com>
2019-09-12 12:09:25 +0100

    I lied there's a second commit

--- code.js
+++ code.js
@@ -1,5 +1,14 @@
-const hash = crypto.createHash('sha256');
-for (let i=0; i < 10e6; i++) {
-    hash.update(randomString())
+const crypto = require('crypto');
+
+function randomString() {
+  return crypto.randomBytes(100).toString('hex');
 }
-res.send(hash.digest('hex') + '\n');
+
+app.get('/compute-sync', function computeSync(req, res) {
+  log('computing sync!');
+  const hash = crypto.createHash('sha256');
+  for (let i=0; i < 10e6; i++) {
+    hash.update(randomString())
+  }
+  res.send(hash.digest('hex') + '\n');
+});
